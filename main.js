b0aecf24513b944fec8f202c817d3bcb9f54dcc3
maycon7771 <108085995+maycon7771@users.noreply.github.com>
2024-03-27 13:40:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ numeroSenha.textContent = tamanhoSenha;
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
-const simbolos = '!@%*?';
+const simbolos = '!@%*?&$+=;:-_¨#';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
@@ -73,7 +73,8 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = "Um computador pode levar até" + Math.floor(2**entropia/
+    (100e6*60*60*24)) + "dias para descobrir essa senha.";
 }
 
 
